3d5439ad7660530a42fd6d62bbb197f1d1459237
Pedro0152 <kresh0152@gmail.com>
2025-09-10 03:36:50 -0300

    Added Double Counter

--- awesome_dashboard/static/src/dashboard/double_counter/double_counter.js
+++ awesome_dashboard/static/src/dashboard/double_counter/double_counter.js
@@ -1,28 +1,27 @@
 /** @odoo-module **/
 
-import { Component, useState, xml } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class DoubleCounter extends Component {
-  static template = xml`
-    <t t-esc="'selected: ' + state.selected + ', value: ' + state[state.selected]"/>
-    <button t-on-click="incrementCounter1">increment count 1</button>
-    <button t-on-click="incrementCounter2">increment count 2</button>
-    <button t-on-click="changeCounter">Switch counter</button>
-  `;
+  static template = "awesome_dashboard.DoubleCounter";
 
   setup() {
-    this.state = useState({ selected: "count1", count1: 0, count2: 0 });
+    this.state = useState({
+      selected: "count1",
+      count1: 0,
+      count2: 0,
+    });
   }
 
-    incrementCounter1() {
-        this.state.selected = "count1";
-        this.state[this.state.selected]++;
-    }
+  incrementCounter1() {
+    this.state.selected = "count1";
+    this.state.count1++;
+  }
 
-    incrementCounter2() {
-        this.state.selected = "count2";
-        this.state[this.state.selected]++;
-    }
+  incrementCounter2() {
+    this.state.selected = "count2";
+    this.state.count2++;
+  }
 
   changeCounter() {
     this.state.selected = this.state.selected === "count1" ? "count2" : "count1";
