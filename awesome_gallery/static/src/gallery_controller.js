d67886eb0fc39b9594a611abf8e2ae57662963fc
Pedro0152 <kresh0152@gmail.com>
2025-09-19 04:10:58 -0300

    4. Load some data.

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,7 +1,8 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { standardViewProps } from "@web/views/standard_view_props";
+import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
@@ -9,5 +10,35 @@ export class GalleryController extends Component {
     static components = { Layout };
     static props = {
         ...standardViewProps,
+        archInfo: Object,
     };
+
+    setup() {
+        this.orm = useService("orm");
+        this.images = useState({ data: [] });
+        onWillStart(async () => {
+            const { records } = await this.loadImages(this.props.domain);
+            this.images.data = records;
+        });
+
+        onWillUpdateProps(async (nextProps) => {
+            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+                const { records } = await this.loadImages(nextProps.domain);
+                this.images.data = records;
+            }
+        });
+    }
+
+    loadImages(domain) {
+        return this.orm.webSearchRead(this.props.resModel, domain, {
+            limit: this.props.archInfo.limit,
+            specification: {
+                [this.props.archInfo.imageField]: {},
+            },
+            context: {
+                bin_size: true,
+            }
+        });
+    }
 }
+
