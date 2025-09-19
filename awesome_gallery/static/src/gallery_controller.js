3f62afc102424de7f4b2cc088f27f7cc0c588c19
Pedro0152 <kresh0152@gmail.com>
2025-09-19 15:13:29 -0300

    10. Add an optional tooltip.

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -21,6 +21,7 @@ export class GalleryController extends Component {
             new this.props.Model(
                 this.orm,
                 this.props.resModel,
+                this.props.fields,
                 this.props.archInfo,
             )
         );
