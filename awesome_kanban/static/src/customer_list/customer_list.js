c0aaaa0ee36ec8b8e317272114732ced8a7ca086
Pedro0152 <kresh0152@gmail.com>
2025-09-20 03:52:49 -0300

    3. Load and display data.

---
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -0,0 +1,17 @@
+import { useService } from "@web/core/utils/hooks";
+import { Component, onWillStart } from "@odoo/owl";
+
+export class CustomerList extends Component {
+    static template = "awesome_kanban.CustomerList";
+    static props = {
+        selectCustomer: {
+            type: Function,
+        },
+    };
+    setup() {
+    this.orm = useService("orm");
+    onWillStart(async () => {
+        this.partners = await this.orm.searchRead("res.partner", [], ["display_name"]);
+        })
+    }
+}
