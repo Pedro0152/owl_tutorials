d5ace9dc3806257d37f2dc126ea820183d3038b7
Pedro0152 <kresh0152@gmail.com>
2025-09-23 12:04:14 -0300

    7. Refactor the code to use t-model.

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -1,6 +1,7 @@
 import { useService } from "@web/core/utils/hooks";
 import { Component, onWillStart, useState} from "@odoo/owl";
 import { KeepLast } from "@web/core/utils/concurrency";
+import { fuzzyLookup } from "@web/core/utils/search";
 
 export class CustomerList extends Component {
     static template = "awesome_kanban.CustomerList";
@@ -14,19 +15,35 @@ export class CustomerList extends Component {
         this.orm = useService("orm");
         this.partners = useState({ data: [] });
         this.keepLast = new KeepLast();
+        this.state = useState({
+            searchString: "",
+            displayActiveCustomers: false,
+        })
 
         onWillStart(async () => {
-            this.partners.data = await this.loadCustomers([]);
+            this.partners.data = await this.loadCustomers();
         })
     }
 
-    async onChangeActiveCustomers(ev) {
-        const checked = ev.target.checked;
-        const domain = checked ?  [["opportunity_ids", "!=", false]] : [];
-        this.partners.data = await this.keepLast.add(this.loadCustomers(domain));
+    get displayedPartners() {
+        return this.filterCustomers(this.state.searchString);
     }
 
-    loadCustomers(domain) {
+    async onChangeActiveCustomers(ev) {
+        this.state.displayActiveCustomers = ev.target.checked;
+        this.partners.data = await this.keepLast.add(this.loadCustomers());
+    }
+
+    filterCustomers(name) {
+        if (name) {
+            return fuzzyLookup(name, this.partners.data, (partner) => partner.display_name);
+        } else {
+            return this.partners.data;
+        }
+    }
+
+    loadCustomers() {
+        const domain = this.state.displayActiveCustomers ? [["opportunity_ids", "!=", false]] : [];
         return this.orm.searchRead("res.partner", domain, ["display_name"]);
     }
 
