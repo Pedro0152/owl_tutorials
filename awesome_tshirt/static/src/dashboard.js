4d39d4aa9244cc114dc1123f5e0ff4882f685198
Pedro0152 <kresh0152@gmail.com>
2025-10-06 02:14:40 -0300

    otm confirmation.

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -2,10 +2,22 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useSearchModel } from "@web/search/search_model";
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    static template = "awesome_tshirt.clientaction";
+    static components = { Layout };
 
-AwesomeDashboard.components = {};
-AwesomeDashboard.template = "awesome_tshirt.clientaction";
+    setup() {
+        this.searchModel = useSearchModel("res.partner"); // modelo para el control panel
+        this.display = {
+            controlPanel: {
+                "top-right": false,
+                "bottom-right": false,
+            },
+        };
+    }
+}
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
